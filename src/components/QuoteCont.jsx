04995da9d47ed55da44fff7756fe420591daffe9
trevornei <117675657+trevornei@users.noreply.github.com>
2023-11-30 13:45:59 -0700

    Added state and get request to advice container.

--- src/components/QuoteCont.jsx
+++ src/components/QuoteCont.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
+/*This container sets the background for: 
+  - QuoteNumber
+  - QuoteBody
+  - DiceButton
+
+  State AND GET request will in QuoteCont.jsx
+  var for quoteNum
+  var for quoteBody
+
+  From Slip API: 
+  Random Advice GET Request:
+  Returns: 
+    - URL: 
+    - Descripton: 
+    - Parameters: 
+  */
+
 export default function QuoteCont() {
+
+  const [advice, setAdvice] = usestate[{}]
+
+  const URL = 'https://api.adviceslip.com/advice'
+
+  const getAdvice = () => {
+    fetch(URL)
+      .then(data => data.json())
+      .then(data => {
+        setAdvice(data)
+        console.log(data)
+      })
+  }
+
   return (
     <>
-        <div className=" w-33.75 h-22.75 bg-darkishBlue rounded-3xl">
+        <div className="w-33.75 h-22.75 bg-darkishBlue rounded-3xl">
 
         </div>
     </>
