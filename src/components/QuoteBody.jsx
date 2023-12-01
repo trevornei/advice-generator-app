d6e5ec515d7278e9140137bb8ab0c43fd5f9984d
trevornei <117675657+trevornei@users.noreply.github.com>
2023-11-30 21:49:15 -0700

    Changed hightlighted text color.

--- src/components/QuoteBody.jsx
+++ src/components/QuoteBody.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function QuoteBody({ adviceBody }) {
     return(
-        <div className="flex flex-col items-center justify-center text-3xl font-semibold text-center border-white font-manrope text-lightCyan md:w-80 md:h-48 sm:mt-4 md:mt-0">"{adviceBody && adviceBody.advice}"</div>
+        <div className="flex flex-col items-center justify-center text-3xl font-semibold text-center border-white font-manrope text-lightCyan md:w-80 md:h-48 sm:mt-4 md:mt-0 selection:bg-neonGreen selection:text-darkBlue">"{adviceBody && adviceBody.advice}"</div>
     )
 }
--- src/components/QuoteNum.jsx
+++ src/components/QuoteNum.jsx
@@ -4,7 +4,7 @@ export default function QuoteNum({ quoteNum }) {
     return(
         <>
             <div className="">
-                <h3 className="mt-12 mb-4 font-normal md:mb-0 sm:mb-0 md:mt-24 lg:mt-24 xl:mt-24 2xl:mt-24 tracking-extraWide font-manrope leading-2 text-neonGreen">ADVICE  #{quoteNum}</h3>
+                <h3 className="mt-12 mb-4 font-normal md:mb-0 sm:mb-0 md:mt-24 lg:mt-24 xl:mt-24 2xl:mt-24 tracking-extraWide font-manrope leading-2 text-neonGreen selection:bg-neonGreen selection:text-darkBlue">ADVICE  #{quoteNum}</h3>
             </div>
         </>
     )
